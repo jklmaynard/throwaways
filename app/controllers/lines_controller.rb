bdd953c786f0e69490ab174e84921601d733fb9c
James Maynard <jameskirkmaynard@gmail.com>
2016-09-30 08:22:17 -0700

    controllers, routes, seeds, models, first layouts

---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,3 @@
+class LinesController < ApplicationController
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+poem1 = Poem.create(name: 'Poem Number One')
+poem2 = Poem.create(name: 'Poem Number the second')
+
+line1 = Line.create(line: 'This is the first line of poem one', poem_id: 1)
+line2 = Line.create(line: 'This is the second line', poem_id: 1)
+line3 = Line.create(line: 'This is the first line poem the second', poem_id: 2)
