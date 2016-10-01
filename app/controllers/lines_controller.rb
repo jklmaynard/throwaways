a552984ef796a13a74729a875c87e11bd9f931ef
James Maynard <jameskirkmaynard@gmail.com>
2016-10-01 07:49:42 -0700

    Add new poems and lines

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,11 +1,17 @@
 class LinesController < ApplicationController
   def create
-    @poem = Poem.find(params[:id])
+    target = params[:poem_id]
+    @poem = Poem.find {|s| s.slug == params[:poem_id]}
     @line = @poem.lines.create(line_params)
+    if @line.save
+      redirect_to poem_path(@line.poem)
+    else
+      redirect_to 'back'
+    end
   end
 
   private
   def line_params
-    params.require(:line).permit(:line, :poem_id)
+    params.require(:line).permit(:line, :poem_id, :stanza_number, :line_number)
   end
 end
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,6 +4,7 @@ class PoemsController < ApplicationController
   end
   def show
     @poem = Poem.friendly.find(params[:id])
+    @line = Line.new
   end
   def new
     @poem = Poem.new
@@ -11,7 +12,7 @@ class PoemsController < ApplicationController
   def create
     @poem = Poem.create(poem_params)
     if @poem.save
-      redirect_to '/'
+      redirect_to poem_path(@poem)
     else
       redirect_to :back
     end
