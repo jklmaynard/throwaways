4184124ec7966035d075b0166380011319ad7f83
James Maynard <jameskirkmaynard@gmail.com>
2016-09-30 21:21:26 -0700

    write the poems/lines create methods

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,3 +1,11 @@
 class LinesController < ApplicationController
+  def create
+    @poem = Poem.find(params[:id])
+    @line = @poem.lines.create(line_params)
+  end
 
+  private
+  def line_params
+    params.require(:line).permit(:line, :poem_id)
+  end
 end
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,11 +1,23 @@
 class PoemsController < ApplicationController
-
   def index
     @poems = Poem.all
   end
-
   def show
     @poem = Poem.friendly.find(params[:id])
   end
+  def new
+    @poem = Poem.new
+  end
+  def create
+    @poem = Poem.create(poem_params)
+    if @poem.save
+      redirect_to '/'
+    else
+      redirect_to :back
+  end
 
+  private
+  def poem_params
+    params.require(:poem).permit(:name)
+  end
 end
